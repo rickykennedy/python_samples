4b0bb23d9d06f81a6c346bd48cd8bc87b749f7cd
ricky@office-mbpro <ricky@yellowcube.com>
2017-02-10 12:02:59 +0800

    adding sampel 3

---
+++ sample2.py
@@ -0,0 +1,34 @@
+from __future__ import division
+
+print(1 + 2);
+# print(float(3)/2);
+print (3/2);
+
+print(4**5);
+
+a = 5;
+print(a+a);
+a=20;
+print(a);
+
+my_income = 100;
+tax_rate = 0.1;
+my_taxes = my_income * tax_rate;
+print(my_taxes);
+print("'hello'");
+print('"hellos"');
+some_text = 'helloworld 1\nhelloworld 2';
+print (some_text);
+print(len(some_text));
+print(some_text[5:]);
+print(some_text[-1]);
+print(some_text[1]);
+print(some_text[:1]);
+print(some_text[::1]);
+print(some_text[::2]);
+print(some_text[::-1]);
+
+some_text.;
+# for(i=0; i < len(some_text); i++){
+#     print(some_text[i]);
+# }
---
+++ sample3.py
@@ -0,0 +1,1 @@
+print("sample 3 for python");
