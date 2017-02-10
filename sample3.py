59b4645ec540871fab6e9d62ba72aa72d498ce90
ricky@office-mbpro <ricky@yellowcube.com>
2017-02-10 15:13:55 +0800

    update

--- sample3.py
+++ sample3.py
@@ -7,3 +7,9 @@ print("need to change this");
 print("can this work");
 
 print("all conflict solve");
+import datetime
+print(
+    datetime.datetime.fromtimestamp(
+        int("1486706279")
+    ).strftime('%Y-%m-%d %H:%M:%S')
+)
